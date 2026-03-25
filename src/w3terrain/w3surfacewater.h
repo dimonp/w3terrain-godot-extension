5c054c81ce768001f83efc8c18ca2e4e10e42ecf
Dmitry A. Peter <dmitrypeter@gmail.com>
2026-03-25 16:23:46 +0600

    Minor fixes.

--- src/w3terrain/w3surfacewater.h
+++ src/w3terrain/w3surfacewater.h
@@ -31,7 +31,7 @@ private:
     static constexpr auto kStatWaterTilesPrecachedId = "W3Terrain/stat_water_tiles_precached";
 #endif
 
-    static constexpr int32_t kMaxGPUMeshes = 0;//godot::RenderingServer::MAX_MESH_SURFACES / 8;
+    static constexpr int32_t kMaxGPUMeshes = godot::RenderingServer::MAX_MESH_SURFACES / 8;
 
     struct CachedVertex {
         math::vector3 pos;
