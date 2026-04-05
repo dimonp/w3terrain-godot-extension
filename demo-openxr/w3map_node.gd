d081fff208f8fa61e7f1a4217c25db11eb5862dc
Dmitry A. Peter <dmitrypeter@gmail.com>
2026-04-05 18:46:46 +0600

    Add ray cell selector to OpenXR demo.

--- demo-openxr/w3map_node.gd
+++ demo-openxr/w3map_node.gd
@@ -6,14 +6,6 @@ signal selector_changed(coords: Vector2i)
 @onready var collision_node = $StaticBody3D
 var selected_coords
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT and event.pressed:
-			var cell_coords = map.pick_cell_by_screen_position(event.position)
-			if cell_coords is Vector2i:
-				selected_coords = cell_coords
-				selector_changed.emit(cell_coords)
-
 func _ready():
 	print("Map node ready: ", name)
 
@@ -25,3 +17,14 @@ func _on_map_initialized(_map_node: Object) -> void:
 func _on_map_info_ui_button_pressed_create_map(id: int) -> void:
 	var size := Vector2i(33, 33)
 	editor.create_map(size)
+
+func update_selector(ray_origin: Vector3, ray_direction: Vector3):
+	print("ray_origin: %s, ray_direction: %s" % [ray_origin, ray_direction])
+	var cell_coords = map.pick_cell_by_ray(ray_origin, ray_direction)
+	print("cell_coords: %s" % [cell_coords])
+	if cell_coords is Vector2i:
+		selected_coords = cell_coords
+		selector_changed.emit(cell_coords)
+		
+func cell_bbox(coords: Vector2i):
+	return map.get_cell_bbox(coords)
--- demo-openxr/xr_origin_3d.gd
+++ demo-openxr/xr_origin_3d.gd
@@ -3,12 +3,29 @@ extends XROrigin3D
 @export var speed: float = 128.0
 
 @onready var camera := $XRCamera3D
+@onready var left_controller := $XRController3D_Left
 @onready var right_controller := $XRController3D_Right
 
+@onready var pointer = $XRController3D_Right/FunctionPointer
+@onready var raycast = $XRController3D_Right/FunctionPointer/RayCast3D
+
+var can_trigger: bool = true
+
 func _physics_process(delta: float):
-	var input = right_controller.get_vector2(&"move")
+	var trigger_val = right_controller.get_float("fire")
+	if can_trigger and trigger_val > 0.5:
+		can_trigger = false
+		_update_selector()
+	elif trigger_val == 0:
+		can_trigger = true
+
+	var input = left_controller.get_vector2(&"move")
 	if input.length() > 0.1:
 		var direction = (camera.global_transform.basis.z * -input.y + 
-					 camera.global_transform.basis.x * input.x)
-
+				camera.global_transform.basis.x * input.x)
 		global_position += direction * speed * delta
+
+func _update_selector():
+	var ray_origin = pointer.global_position
+	var ray_direction = -pointer.global_transform.basis.z.normalized()
+	%W3MapNode.update_selector(ray_origin, ray_direction)
